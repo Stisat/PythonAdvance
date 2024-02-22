00e64ad48b45169f00a4e579670c330d65d3775b
Ream <reamtr25@gmail.com>
2024-02-22 20:55:23 +0300

    HW15

--- main.py
+++ main.py
@@ -1,100 +1,43 @@
-from random import randint as rnd
+from collections import namedtuple
+import os
+import logging
+import argparse
+
+FORMAT = '{levelname} - {asctime}. В модуле создана запись функцией: {funcName}():\n{msg} '
+logging.basicConfig(format=FORMAT, style='{', filename='log.txt', filemode='w', encoding='utf-8', level=logging.INFO)
+
+files_t = namedtuple('files_t', 'name, ext_flag, path')
 
 
-def check_h_v(chess_desk: list):
-    """Checking for "1" value vertically and horizontally"""
-    temp_H_V = []
-    for column in range(len(chess_desk)):
-        for raw in range(len(chess_desk[0])):
-            if chess_desk[column].count(1) > 1:
-                return False
-            temp_H_V.append(chess_desk[raw][column])
-        if temp_H_V.count(1) > 1:
-            return False
-        temp_H_V.clear()
-    return True
+def get_dir_structure(path: str):
+    """
+    Функция для получения списков файлов и директорий внутри указанного пути path
+    в формате X:/Dir/Dir/Dir
+    """
+    res = []
+    for path, file_name, dir_name in os.walk(path):
+        res.append((path, file_name, dir_name))
+    return res[0]
 
 
-def check_diag_l(desk_chess: list):
-    """Checking diagonals from lower left to upper right"""
-    i = 6
-    size = len(desk_chess)
-    temp_d = []
-    while i >= 0:
-        for raw2 in range(size - i):
-            temp_d.append(desk_chess[raw2 + i][raw2])
-        if temp_d.count(1) > 1:
-            return False
-        temp_d.clear()
-        i -= 1
-    j = 1
-    while j < size:
-        for rawm in range(size - j):
-            temp_d.append(desk_chess[rawm][rawm + j])
-        if temp_d.count(1) > 1:
-            return False
-        temp_d.clear()
-        j += 1
-    return True
+def set_named(path: str):
+    """
+     Функция заполняет объекты namedtuple с последующим выводом структуры
+    """
+    col = list(get_dir_structure(path))
+    data_named = []
+    curr_dir = str(col[0]).split('/')[-1]
+    for name in col[1]:
+        data_named.append(files_t(name, 'Dir', curr_dir))
+    for name in col[2]:
+        parts_name = str(name).split('.')
+        data_named.append(files_t(parts_name[0], parts_name[1], curr_dir))
+    logging.info('\n'.join(f'{i}' for i in data_named))
+    return '\n'.join(f'{i}' for i in data_named)
 
 
-def check_diag_r(desk_chess: list):
-    """Checking diagonals from bottom right to top left"""
-    i = 5
-    size = len(desk_chess)
-    temp_d = []
-    while i >= -1:
-        for raw1 in range(size - 1, i, -1):
-            temp_d.append(desk_chess[raw1][i - raw1])
-        if temp_d.count(1) > 1:
-            return False
-        temp_d.clear()
-        i -= 1
-    j = 2
-    while j < size:
-        for raw3 in range(size - j + 1):
-            temp_d.append(desk_chess[raw3][- j - raw3])
-        if temp_d.count(1) > 1:
-            return False
-        temp_d.clear()
-        j += 1
-    return True
-
-
-def is_attacking(q_1, q_2):
-    """Checking whether the queens beat each other"""
-    desk = [[0] * 8 for _ in range(8)]
-    desk[q_1[0] - 1][q_1[1] - 1] = 1
-    desk[q_2[0] - 1][q_2[1] - 1] = 1
-    if check_h_v(desk) == check_diag_l(desk) == check_diag_r(desk):
-        return True
-    else:
-        return False
-
-
-def check_queens(queens: list):
-    result = []
-    for i in range(len(queens)):
-        for j in range(i + 1, len(queens)):
-            result.append(is_attacking(queens[i], queens[j]))
-    if False in result:
-        return False
-    else:
-        return True
-
-
-def generate_boards():
-    result = []
-    count = 0
-    attempts = 4
-    while count < attempts:
-        board = ((rnd(1, 8), rnd(1, 8)) for _ in range(1, 9))
-        temp = [*board]
-        if check_queens(temp) is True:
-            result.append(temp)
-            count += 1
-    return result
-
-
-board_list = generate_boards()
-print(board_list)
+if __name__ == '__main__':
+    pars = argparse.ArgumentParser(description='Получение структуры каталога: ')
+    pars.add_argument('path', metavar='P', type=str, help='Enter absolute path to directory')
+    arg = pars.parse_args()
+    print(set_named(arg.path))
